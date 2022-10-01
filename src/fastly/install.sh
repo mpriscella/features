953ffd58971187b8217feea0b2555b957e11457e
Mike Priscella <mpriscella@gmail.com>
2022-10-01 03:02:37 +0000

    Fixed fastly feature

--- src/fastly/install.sh
+++ src/fastly/install.sh
@@ -11,14 +11,15 @@ echo "Installing Fastly version ${versionStr}"
 
 architecture=$(dpkg --print-architecture)
 case "${architecture}" in
-	amd64) architectureStr=amd64 ;;
+	amd64) architectureStr=386 ;;
 	arm64) architectureStr=arm64 ;;
 	*)
 		echo "Fastly does not support machine architecture '$architecture'. Please use an x86-64 or ARM64 machine."
 		exit 1
 esac
 
-curl -L "https://github.com/fastly/cli/releases/download/v${versionStr}/fastly_${versionStr}_linux_${architectureStr}.deb" \
-	-o /usr/local/bin/fastly
+curl -L "https://github.com/fastly/cli/releases/download/${versionStr}/fastly_${versionStr}_linux-${architectureStr}.tar.gz" \
+	-o fastly.tar.gz
 
-chmod +x /usr/local/bin/fastly
+tar -xzf fastly.tar.gz -C /usr/local/bin/
+rm fastly.tar.gz
--- test/fastly/test.sh
+++ test/fastly/test.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "version" fastly --version
+check "version" fastly version
 
 # Report result
 reportResults
